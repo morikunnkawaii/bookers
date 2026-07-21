6b6ffc46411aab0adf7e729da163ba808d22703d
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-07-21 06:30:11 +0000

    [Add]create追加途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
+    @book = Book.new <!-- インスタンス変数 -->
   end
+
+  def create
+    book = Book.new(list_params)
+
 
   def index
   end
