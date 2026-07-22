adbe5b2d67247e8484dfd4efe8930ce5674d34dd
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-07-22 06:23:24 +0000

    [Add]ルートディレクトリ作成,メモ＜！－－を＃へ,index,html記述途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,17 @@
 class BooksController < ApplicationController
   def new
-    @book = List.new 
+    @book = Book.new 
   end
 
   def create
-    book = List.new(list_params)     #データ受け取り新規作成インスタンス作成
-    book.save                    <!-- データをデータベースに保存 -->
-    redirect_to '/'                 #top画面へ
+    book = Book.new(list_params)     #データ受け取り新規作成インスタンス作成
+    book.save                        #データをデータベースに保存
+    redirect_to root_path                  #top画面へ
   end
    
 
   def index
+    @books = Book.all  #テーブルに保存されたデータを全て取得
   end
 
   def show
@@ -22,4 +23,5 @@ class BooksController < ApplicationController
   private #ストロングパラメータでセキュリティ強化
   def list_params
     params.require(:book).permit(:title, :body)
+  end  
 end
