8d5a1608dab20ec46d263446d1c335dab2e2df10
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-07-25 12:58:49 +0000

    [add]showへフラッシュメッセージ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,23 @@
 class BooksController < ApplicationController
 
-  def create
-    @book = Book.new(list_params)     #データ受け取り新規作成インスタンス作成
-    if @book.save                        #データをデータベースに保存
-      redirect_to book_path(@book.id)                  #showへ
-    else
-      @books = Book.all
-      render :index, status: :uprocessable_entity
-    end
-  end
-   
-
   def index
     @book = Book.new
     @books = Book.all  #テーブルに保存されたデータを全て取得
   end
+
+  def create
+    @book = Book.new(list_params)     #データ受け取り新規作成インスタンス作成
+    if @book.save                        #データをデータベースに保存
+      redirect_to book_path(@book.id)                  #showへ
+      flash[:notice] = "Book was successfully created"  #フラッシュメッセージ redirect_toの上にするとなぜか投稿できなかった。上から処理されるうえで何か問題が生じるのだろう。
+    else
+      @books = Book.all                             #indexのインスタンス変数
+      render :index, status: :unprocessable_entity  #indexへ
+    end
+  end
+   
+
+
 
   def show
     @book = Book.find(params[:id])
