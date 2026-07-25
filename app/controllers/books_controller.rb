e3c744b325ae2d598d1c5660869fe3530a219659
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-07-25 09:05:51 +0000

    [add]各種リンク追加,表記変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,13 +23,13 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(list_params)
-    redirect_to book_path(book:id)
+    redirect_to book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to "/books"
+    redirect_to books_path
   end
 
   private #ストロングパラメータでセキュリティ強化
